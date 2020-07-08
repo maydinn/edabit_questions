6257108a6e5814b576e2c99a3a5553255cd434e7
aydin <muhammedaydin9214@gmail.com>
2020-07-08 11:19:13 +0200

    count_characters

---
+++ edabit/count_chc.py
@@ -0,0 +1,15 @@
+def count_characters(lst):
+    """"
+    a function that counts how many characters make up a rectangular shape. You will be given a list of strings.
+    """
+    word = ""
+    for i in range(0, len(lst)):
+        word += lst[i]
+    return len(word)
+
+
+print(count_characters([
+    "###",
+    "###",
+    "###"
+]))
