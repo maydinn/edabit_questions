b39dc42dbe160422d1b3c6931e7d9c1ffa5ce763
aydin <muhammedaydin9214@gmail.com>
2020-07-09 09:32:03 +0200

    edabit

---
+++ edabit/is_curzon.py
@@ -0,0 +1,6 @@
+def is_curzon(num):
+    """
+    a function that returns True if num is a Curzon number, or False otherwise.
+    If 1 plus 2 elevated to num is exactly divisible by 1 plus 2 multiplied by num, then num is a Curzon number.
+    """
+    return (2 ** num + 1) % (2 * num + 1) == 0
