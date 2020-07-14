4b17e1cf86cdd2af78b72bb09a1c360b7df166de
aydin <muhammedaydin9214@gmail.com>
2020-07-14 12:33:07 +0200

    practice

---
+++ edabit/second/square_areas_difference.py
@@ -0,0 +1,7 @@
+def square_areas_difference(r):
+    """
+     a function, that takes an integer (radius of the circle) and returns the difference of the areas of the two squares
+     Imagine a circle and two squares: a smaller and a bigger one.
+     For the smaller one, the circle is a circumcircle and for the bigger one, an incircle.
+    """
+    return (r ** 2) * 2
